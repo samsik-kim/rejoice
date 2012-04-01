9bb644ae06f8ac410751645a88c1dfaccfd8a795
teributter <teributter@83ac92b2-790b-7805-d4d2-1d3fbb1524ae>
2012-04-01 21:36:34 +0000

    work_

--- trunk/stockinvest/src/com/stockinvest/data/stockinvest/dao/impl/StockinvestDAOImpl.java
+++ trunk/stockinvest/src/com/stockinvest/data/stockinvest/dao/impl/StockinvestDAOImpl.java
@@ -7,14 +7,31 @@ import org.springframework.stereotype.Repository;
 import tframe.core.dao.IBatisAbstractDao;
 
 import com.stockinvest.data.stockinvest.dao.StockinvestDAO;
+import com.stockinvest.data.stockinvest.info.MemberInfo;
 
 @Repository
 public class StockinvestDAOImpl extends IBatisAbstractDao implements StockinvestDAO{
 	
-//	private static final String NAME_SPACE = "stockinvest";
+	private static final String NAME_SPACE = "stockinvest";
 
 	@Override
 	public int selectEmp() throws SQLException {
 		return (Integer)getSqlMapClient().queryForObject("stockinvest.selectEmp");
+	}
+
+	/**
+	 * 관리자 정보 가져오기
+	 */
+	@Override
+	public MemberInfo selectAdminInfo() throws SQLException {
+		return (MemberInfo)getSqlMapClient().queryForObject(NAME_SPACE + ".selectMemberInfo"); 
+	}
+
+	/**
+	 * 비밀번호 변경
+	 */
+	@Override
+	public int updatePassword(MemberInfo info) throws SQLException {
+		return getSqlMapClient().update(NAME_SPACE + ".update", info);
 	}
 }
